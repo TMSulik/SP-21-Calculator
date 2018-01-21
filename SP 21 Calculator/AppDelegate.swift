0089e5b4731d60697d07433b4c4acbc18e3b25e6
TMSulik <shulich@hotmail.com>
2018-01-20 18:15:57 -0800

    First attempt

--- SP 21 Calculator/AppDelegate.swift
+++ SP 21 Calculator/AppDelegate.swift
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print("Application did finish launching")
         return true
     }
 
