1c0a318380cdd4728415c87a8507de4f04c2b9fd
TMSulik <shulich@hotmail.com>
2018-01-20 17:38:41 -0800

    Initial Commit

---
+++ SP 21 Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SP 21 Calculator
+//
+//  Created by Tom Sulik on 1/20/18.
+//  Copyright © 2018 RT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
